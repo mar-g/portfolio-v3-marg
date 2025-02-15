7d304f9233e59bc55a5378ce78e96b95dba4fb53
Marcin G. <45784184+mar-g@users.noreply.github.com>
2025-02-15 19:01:51 +0100

    commit 07

---
+++ components/Skills.js
@@ -0,0 +1,17 @@
+import Heading from "./Heading";
+import SectionWrapper from "./SectionWrapper";
+import data from "@/data/skills";
+import SkillComponent from "./SkillComponent";
+
+export default function Skills() {
+  return (
+    <SectionWrapper>
+      <Heading>skills</Heading>
+      <div className="flex flex-wrap items-center gap-5">
+        {data.map((skill) => (
+          <SkillComponent skill={skill} key={skill.id} />
+        ))}
+      </div>
+    </SectionWrapper>
+  );
+}
