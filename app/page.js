e35ee42da9594cb6e5783a9fc1d9dbc1a3196f6e
Marcin G. <45784184+mar-g@users.noreply.github.com>
2025-02-19 17:33:10 +0100

    commit 12

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Contacts from "@/components/Contacts";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Projects from "@/components/Projects";
 import Reviews from "@/components/Reviews";
@@ -7,15 +8,18 @@ import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main className="max-w-screen-xl w-full mx-auto px-4">
-      <div className="mt-20">
-        <Header />
-        <Contacts />
-        <About />
-        <Skills />
-        <Projects />
-        <Reviews />
-      </div>
-    </main>
+    <>
+      <main className="max-w-screen-xl w-full mx-auto px-4">
+        <div className="mt-20">
+          <Header />
+          <Contacts />
+          <About />
+          <Skills />
+          <Projects />
+          <Reviews />
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }
---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-t-zinc-100 dark:border-t-zinc-800 mt-10">
+      <div className="max-w-screen-xl mx-auto px-4 py-5">
+        <p className="text-sm font-semibold text-center text-zinc-800 dark:text-zinc-400 capitalize">
+          Lorem ipsum dolor <strong className="font-black">sit amet.</strong>
+        </p>
+      </div>
+    </footer>
+  );
+}
